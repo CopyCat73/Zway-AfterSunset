f47779ed9f44b7c3a475ac1c3466fe1c638de474
CopyCat73 <copycat73@outlook.com>
2015-12-16 20:57:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Zway AfterSunset
+
+Zway Automation module for switching devices a set period after sunset
+
+# Configuration
+
+This module uses the Daylight app as trigger, please install it first. 
+
+
+# License
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or any 
+later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
---
+++ index.js
@@ -0,0 +1,82 @@
+/*** AfterSunset Z-Way module *******************************************
+
+Version: 1.00
+(c) CopyCatz, 2015
+-----------------------------------------------------------------------------
+Author: CopyCatz <copycat73@outlook.com>
+Description: Light switching at set interval after sunset.
+
+******************************************************************************/
+
+function AfterSunset (id, controller) {
+    // Call superconstructor first (AutomationModule)
+    AfterSunset.super_.call(this, id, controller);
+    
+
+}
+
+inherits(AfterSunset, AutomationModule);
+
+_module = AfterSunset;
+
+// ----------------------------------------------------------------------------
+// --- Module instance initialized
+// ----------------------------------------------------------------------------
+
+AfterSunset.prototype.init = function (config) {
+    AfterSunset.super_.prototype.init.call(this, config);
+    
+    this.controller.on("daylight.sunset", this.scheduleDevices);
+       
+};
+    
+
+
+AfterSunset.prototype.stop = function () {
+    
+    var self = this;
+    
+    this.controller.off("daylight.sunset", this.scheduleDevices);
+    
+    AfterSunset.super_.prototype.stop.call(this);
+ 
+};
+
+// ----------------------------------------------------------------------------
+// --- Module methods
+// ----------------------------------------------------------------------------
+
+AfterSunset.prototype.scheduleDevices = function () {
+    var self = this;
+
+    _.each(self.config.onofflights,function(element) {
+        var vDev = self.controller.devices.get(element.device);
+        if (vDev) {
+            var timeout = element.delay*60000;
+            setTimeout(
+            _.bind(self.switchDevice,self,vDev,'on'),
+            timeout);
+        }
+    });
+            
+    _.each(self.config.dimmers,function(element) {
+        var vDev = self.controller.devices.get(element.device);
+        if (vDev) {
+            var timeout = element.delay*60000;
+            setTimeout(
+            _.bind(self.switchMode,self,vDev,element.level),
+            timeout);
+        }
+    });    
+}
+
+AfterSunset.prototype.switchDevice = function (device,state) {
+    var self = this;
+    if (state === 'on') {
+       device.performCommand("on");
+    }
+    else {
+        device.set("metrics:level",state);
+    }
+
+}
